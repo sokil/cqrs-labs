5ec7a139449e0193e9636a53e472ac488e1bb999
Dmytro Sokil <dmytro.sokil@gmail.com>
2020-11-14 17:41:11 +0200

    asdd balance

--- src/Prooph/Command/AddBalanceOperation/AddBalanceOperationHandler.php
+++ src/Prooph/Command/AddBalanceOperation/AddBalanceOperationHandler.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace CqrsLabs\Prooph\Command\AddBalanceOperation;
 
+use CqrsLabs\Prooph\Aggregate\Balance;
+use CqrsLabs\Prooph\ValueObject\BalanceOperationId;
+use CqrsLabs\Prooph\ValueObject\BalanceOperationType;
+use CqrsLabs\Prooph\ValueObject\CreditsAmount;
+
 class AddBalanceOperationHandler
 {
     public function __invoke(AddBalanceOperation $addBalanceOperation): void
     {
-        echo $addBalanceOperation->getUserId();
+        $balance = Balance::initialiseBalance($addBalanceOperation->getUserId());
+
+        $balance->addOperation(
+            new BalanceOperationId(1),
+            BalanceOperationType::ADD_CREDITS(),
+            new CreditsAmount(10)
+        );
+
+        echo 'Actual balance: ' . $balance->getAmount()->getIntValue() . PHP_EOL;
     }
 }
